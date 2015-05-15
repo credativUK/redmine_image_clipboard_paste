19fa5ac36187804f54ea5b47a6a70d65338d60b3
Thorin <thorin.iv@gmail.com>
2015-05-15 10:37:44 +0100

    Fix paste when the user can only add notes

--- lib/image_clipboard_paste/hooks.rb
+++ lib/image_clipboard_paste/hooks.rb
@@ -1,10 +1,8 @@
 module ImageClipboardPaste
   class Hooks < Redmine::Hook::ViewListener
-    render_on :view_issues_form_details_bottom, :partial => 'imagepaste'
-
     def view_layouts_base_body_bottom(context={})
       controller = context[:controller]
-      if controller.is_a?(BoardsController) || controller.is_a?(MessagesController) || controller.is_a?(WikiController) || controller.is_a?(NewsController)
+      if controller.is_a?(IssuesController) || controller.is_a?(BoardsController) || controller.is_a?(MessagesController) || controller.is_a?(WikiController) || controller.is_a?(NewsController)
 
         javascript_include_tag 'image_paste.js', :plugin => 'redmine_image_clipboard_paste'
 
