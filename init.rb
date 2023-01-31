95903f1cab4803265675a5d3eb08121b850c17dc
Markus <markus.zipfel@aimcom.de>
2023-01-31 12:30:17 +0100

    [TASK] Redmine 5.0.4 compatibility
    Updated requirement of "image_clipboard_paste/hooks" in `init.rb`

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 require 'redmine'
-require 'image_clipboard_paste/hooks'
+require File.expand_path('lib/image_clipboard_paste/hooks', __dir__)
 
 Redmine::Plugin.register :redmine_image_clipboard_paste do
   name 'Image Clipboard Paste'
